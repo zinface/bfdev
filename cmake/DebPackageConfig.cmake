95b24c5fe81b87070b1a36ecb2a9cc324f039288
zinface <zinface@163.com>
2023-03-04 23:03:09 +0800

    cmake: Remove all Chinese comments

--- cmake/DebPackageConfig.cmake
+++ cmake/DebPackageConfig.cmake
@@ -1,24 +1,5 @@
 cmake_minimum_required(VERSION 3.0.0)
 
-# function(add_deb_package PACKAGE_NAME PACKAGE_VERSION PACKAGE_MAINTAINER PACKAGE_EMAIL PACKAGE_SHORT_DESCRIPTION PACKAGE_LONG_DESCRIPTION)
-    
-# endfunction(add_deb_package PACKAGE_NAME PACKAGE_VERSION PACKAGE_MAINTAINER PACKAGE_EMAIL PACKAGE_SHORT_DESCRIPTION PACKAGE_LONG_DESCRIPTION)
-
-# if(add_deb_package    VALUE) set(Package    ${VALUE} PARENT_SCOPE) endif(add_deb_package    VALUE)
-# if(add_deb_version    VALUE) set(Version    ${VALUE} PARENT_SCOPE) endif(add_deb_version    VALUE)
-# if(add_deb_maintainer VALUE) set(Maintainer ${VALUE} PARENT_SCOPE) endif(add_deb_maintainer VALUE)
-# if(add_deb_email      VALUE) set(Email      ${VALUE} PARENT_SCOPE) endif(add_deb_email      VALUE)
-# if(add_deb_descrition VALUE) set(Descrition ${VALUE} PARENT_SCOPE) endif(add_deb_descrition VALUE)
-# if(add_deb_detail     VALUE) set(Detail     ${VALUE} PARENT_SCOPE) endif(add_deb_detail     VALUE)
-
-
-# set(Package         "")
-# set(Version         "")
-# set(Architecture    "")
-# set(Maintainer      "")
-# set(Email           "")
-# set(Descrition      "")
-
 function(find_str _IN _SEP _OUT)
     string(FIND "${_IN}" "${_SEP}" _TMP)
     set(${_OUT} ${_TMP} PARENT_SCOPE)
@@ -99,7 +80,6 @@ function(set_package_vars _IN_KEY _IN_VAL)
         string(TOUPPER "${_IN_VAL}" _IN_VAL_UPPER)
         string(TOLOWER "${_IN_VAL}" _IN_VAL_LOWER)
         set(CPACK_GENERATOR "${_IN_VAL_UPPER}" PARENT_SCOPE)
-        message("--> 软件包类型: ${_IN_VAL_LOWER}")
     endif(_Type EQUAL "0")
 
     find_str("${_IN_KEY}" "Package" _Package)
@@ -109,7 +89,6 @@ function(set_package_vars _IN_KEY _IN_VAL)
         else()
             set(CPACK_DEBIAN_PACKAGE_NAME "${_IN_VAL}" PARENT_SCOPE)
         endif(_IN_VAL STREQUAL "auto")
-        message("--> 软件包名: ${_IN_VAL}")
     endif(_Package EQUAL "0")
 
     find_str("${_IN_KEY}" "Version" _Version)
@@ -120,13 +99,11 @@ function(set_package_vars _IN_KEY _IN_VAL)
             set(CPACK_DEBIAN_PACKAGE_VERSION "${_IN_VAL}" PARENT_SCOPE)
         endif(_IN_VAL STREQUAL "auto")
         
-        message("--> 软件版本: ${_IN_VAL}")
     endif(_Version EQUAL "0")
 
     find_str("${_IN_KEY}" "CalVer" _CalVer)
     if(_CalVer EQUAL "0")
         set(CalVer "${_IN_VAL}" PARENT_SCOPE)
-        message("--> 日历化版本: ${_IN_VAL}")
     endif(_CalVer EQUAL "0")
 
     find_str("${_IN_KEY}" "Architecture" _Architecture)
@@ -140,43 +117,35 @@ function(set_package_vars _IN_KEY _IN_VAL)
             )
             set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "${_RETV}" PARENT_SCOPE)
         endif(_IN_VAL STREQUAL "auto")
-        message("--> 软件架构: ${_IN_VAL}")
     endif(_Architecture EQUAL "0")
     
     find_str("${_IN_KEY}" "Priority" _Priority)
     if(_Priority EQUAL "0")
         set(CPACK_DEBIAN_PACKAGE_PRIORITY "${_IN_VAL}" PARENT_SCOPE)
-        message("--> 优先级: ${_IN_VAL}")
     endif(_Priority EQUAL "0")
 
     find_str("${_IN_KEY}" "Depends" _Depends)
     if(_Depends EQUAL "0")
         set(CPACK_DEBIAN_PACKAGE_DEPENDS "${_IN_VAL}" PARENT_SCOPE)
-        message("--> 软件依赖: ${_IN_VAL}")
     endif(_Depends EQUAL "0")
 
     find_str("${_IN_KEY}" "Maintainer" _Maintainer)
     if(_Maintainer EQUAL "0")
         set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${_IN_VAL}" PARENT_SCOPE)
-        message("--> 软件维护者: ${_IN_VAL}")
     endif(_Maintainer EQUAL "0")
 
     find_str("${_IN_KEY}" "Homepage" _Homepage)
     if(_Homepage EQUAL "0")
         set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "${_IN_VAL}" PARENT_SCOPE)
-        message("--> 软件主页: ${_IN_VAL}")
     endif(_Homepage EQUAL "0")
 
     find_str("${_IN_KEY}" "Recommends" _Recommends)
     if(_Recommends EQUAL "0")
         set(CPACK_DEBIAN_PACKAGE_RECOMMENDS "${_IN_VAL}" PARENT_SCOPE)
-        message("--> 软件建议: ${_IN_VAL}")
     endif(_Recommends EQUAL "0")
     
 endfunction(set_package_vars _IN_KEY _IN_VAL)
 
-# 定义一个自定义(add_package_descript)函数
-# 用于按特定配置约定自动化构建软件包配置
 function(add_package_descript IN_DES)
     set(PACKAGE_DES_PATH "${IN_DES}")
 
@@ -192,20 +161,16 @@ function(add_package_descript IN_DES)
     file(READ ${PACKAGE_DES_PATH} DES_CONTENT)
     trim_str("${DES_CONTENT}" DES_CONTENT)
 
-    ################## 解析 ##################
 
     sub_next(${DES_CONTENT} NEXT_INDEX DES_LINE DES_CONTENT)
     set(PREV_DES "")
     while(NOT DES_LINE STREQUAL "${PREV_DES}")
-        # 检查该描述行是否是 # 注释开头，是的话将跳过该行
         find_str("${DES_LINE}" "#" _COMMENT)
         if(_COMMENT EQUAL "0")
-            message("--> !!!!!!! ${DES_LINE}")
             sub_next(${DES_CONTENT} NEXT_INDEX DES_LINE DES_CONTENT)
             continue()
         endif(_COMMENT EQUAL "0")
 
-        # 检查该描述行是否是 Descrition 开头，是的话说明描述结尾了
         find_str("${DES_LINE}" "Descrition" _DESCRIPTION)
         if(_DESCRIPTION EQUAL "0")
             break()
@@ -223,13 +188,11 @@ function(add_package_descript IN_DES)
             endif(NOT __TMP_LENGTH EQUAL "0")
         endif(_TMP_LEN EQUAL 2)
 
-        # 记录当前行，获取下一行，可能是已经结尾了(将保持重复行)
         set(PREV_DES "${DES_LINE}")
         sub_next(${DES_CONTENT} NEXT_INDEX DES_LINE DES_CONTENT)
     endwhile(NOT DES_LINE STREQUAL "${PREV_DES}")
 
 
-    # 再一次检查该描述行是否是 Descrition 开头，是的话将进行分析描述行
     find_str("${DES_LINE}" "Descrition" _DESCRIPTION)
     if(_DESCRIPTION EQUAL "0")
         split_str_p("${DES_LINE}" ":" _TMP __TMP)
@@ -243,29 +206,22 @@ function(add_package_descript IN_DES)
             set(PREV_DES_LINE "${DES_LINE}")
             sub_next(${DES_CONTENT} NEXT_INDEX DES_LINE DES_CONTENT)
         endwhile(NOT PREV_DES_LINE STREQUAL DES_LINE)
-        # set(Descrition "${Descrition}")
-        message("--> 软件说明: ${Descrition}")
 
         set(CPACK_DEBIAN_PACKAGE_DESCRIPTION ${Descrition})
     endif(_DESCRIPTION EQUAL "0")
 
-    ##################### deb #####################
-    # ARCHITECTURE
     if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
         set(ARCHITECTURE "amd64")
     elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "aarch64")
         set(ARCHITECTURE "arm64")
     endif()
 
-    #################### Calendar Version ###################
     if("${CalVer}" STREQUAL "true")
         string(TIMESTAMP BUILD_TIME "%Y%m%d")
         set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_DEBIAN_PACKAGE_VERSION}-${BUILD_TIME}")
     endif("${CalVer}" STREQUAL "true")
     
 
-
-    ##################### deb file name #####################
     set(_Package      "${CPACK_DEBIAN_PACKAGE_NAME}")
     set(_Version      "${CPACK_DEBIAN_PACKAGE_VERSION}")
     set(_Architecture "${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
@@ -275,49 +231,6 @@ function(add_package_descript IN_DES)
     )
     set(CPACK_DEBIAN_FILE_NAME            ${_DebFileName})
 
-
-    # set(CPACK_DEBIAN_PACKAGE_NAME         "${Package}")
-    # set(CPACK_DEBIAN_PACKAGE_VERSION      "${Version}")
-    # set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "${Architecture}")
-    # set(CPACK_DEBIAN_PACKAGE_DEPENDS      "${Depends}")
-    # set(CPACK_DEBIAN_PACKAGE_PRIORITY     "${Priority}")
-    # set(CPACK_DEBIAN_PACKAGE_MAINTAINER   "${Maintainer}")
-    # set(CPACK_DEBIAN_PACKAGE_DESCRIPTION  "${Descrition}")
-
-    # 设置即将使用的标准脚本
-    # set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
-    #     "${CMAKE_SOURCE_DIR}/config/DEBIAN/preinst"
-    #     "${CMAKE_SOURCE_DIR}/config/DEBIAN/postinst"
-    #     "${CMAKE_SOURCE_DIR}/config/DEBIAN/prerm"
-    #     "${CMAKE_SOURCE_DIR}/config/DEBIAN/postrm"
-    # )
-
-    # 设置为ON，以便使用 dpkg-shlibdeps 生成更好的包依赖列表。
-    # set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
-    # set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS ON)
-    # set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS_POLICY "=")
-
     include(CPack)
 
 endfunction(add_package_descript IN_DES)
-
-
-# TODO:
-# CPACK_GENERATOR
-# CPACK_DEBIAN_FILE_NAME                - n
-# CPACK_DEBIAN_PACKAGE_NAME             - y
-# CPACK_DEBIAN_PACKAGE_VERSION          - y
-# CPACK_DEBIAN_PACKAGE_ARCHITECTURE     - y(auto)
-# CPACK_DEBIAN_PACKAGE_DEPENDS          - y
-# CPACK_DEBIAN_PACKAGE_PRIORITY         - y
-# CPACK_DEBIAN_PACKAGE_MAINTAINER       - y
-# CPACK_DEBIAN_PACKAGE_DESCRIPTION      - y
-
-# ARCHITECTURE
-# if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
-#     set(ARCHITECTURE "amd64")
-# elseif(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "aarch64")
-#     set(ARCHITECTURE "arm64")
-# endif()
-
-# string(TIMESTAMP BUILD_TIME "%Y%m%d")
